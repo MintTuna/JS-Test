ab86ebeff167328e9af6eb864e089f66da9f9536
MintTuna <100212981+MintTuna@users.noreply.github.com>
2024-06-08 16:59:46 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+setInterval(()=>{
+
+let imgs = document.querySelectorAll('img');
+imgs.forEach((a, i)=>{
+    a.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGsD1MXHAP40BvuQ5JM37XGcJrOJkTfNprAg&s'
+})
+
+}, 100)
